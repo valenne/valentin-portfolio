592d2db8f6b52f962af126f6aa40a84da03931e4
Nelson Venegas <nelson.venegasg@gmail.com>
2023-02-05 16:08:17 -0300

    added sticky/fixed navbar with overlay

--- src/layouts/header/SectionHeader.jsx
+++ src/layouts/header/SectionHeader.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavBar } from "./NavBar.jsx";
 
 export const Header = () => {
+  useEffect(() => {
+    window.addEventListener("scroll", isSticky);
+    return () => {
+      window.removeEventListener("scroll", isSticky);
+    };
+  }, []);
+
+  const isSticky = (e) => {
+    const header = document.querySelector(".header-section");
+    const scrollTop = window.scrollY;
+    scrollTop >= 200
+      ? header.classList.add("is-sticky")
+      : header.classList.remove("is-sticky");
+  };
+
   return (
     <section className="header-section">
       <NavBar />
